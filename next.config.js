f4e922e65a365deacf47570edb27ff3a135f49d4
Sol Kim <cjzl123475@gmail.com>
2022-01-30 21:32:53 +0900

    feat: Apply dotenv and .env with prefix 'NEXT'

--- next.config.js
+++ next.config.js
@@ -1,3 +1,22 @@
+const path = require('path');
+const { config } = require('dotenv');
+
+config({
+  path: path.resolve(
+    process.cwd(),
+    process.env.NODE_ENV === 'development'
+      ? './env/.env.development'
+      : './env/.env.production'
+  ),
+});
+
+const prod = process.env.NODE_ENV === 'production';
+
 module.exports = {
+  env: {
+    NEXT_PUBLIC_URL: process.env.NEXT_PUBLIC_URL,
+    NEXT_BACKEND_ADDR: process.env.NEXT_BACKEND_ADDR,
+    NEXT_NAVER_TOKEN_CID: process.env.NEXT_NAVER_TOKEN_CID,
+  },
   reactStrictMode: true,
-}
+};
